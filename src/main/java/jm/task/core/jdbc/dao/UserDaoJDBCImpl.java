e5677cfde07392d3a0936ba2caac3791f4dccb40
AlyaPtr <a.pestrikova@g.nsu.ru>
2023-04-27 19:02:31 +0700

    Solved 2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,23 +11,53 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public UserDaoJDBCImpl(){
     }
+    private void createQuery(String query) {
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public void createUsersTable() {
+        createQuery("CREATE TABLE dbtest.Users (\n" +
+                "`id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "`name` VARCHAR(45) NOT NULL,\n" +
+                "`lastname` VARCHAR(45) NOT NULL,\n" +
+                "`age` INT(3) NOT NULL,\n" +
+                "PRIMARY KEY (`id`));");
     }
 
     public void dropUsersTable() {
+        createQuery("DROP TABLE dbtest.Users;");
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        createQuery("INSERT INTO dbtest.Users (name, lastname, age) VALUES " +
+                "('" + name + "', '" + lastName + "', " + age + ");");
+        System.out.println("User с именем - " + name + " добавлен в базу данных.");
     }
 
     public void removeUserById(long id) {
+        createQuery("DELETE FROM dbtest.Users WHERE id=" + id + ";");
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        String sql = "SELECT * FROM dbtest.Users";
+        try(Statement statement = Util.getConnection().createStatement()) {
+            ResultSet res = statement.executeQuery(sql);
+            while(res.next()) {
+                list.add(new User(res.getString("name"),
+                        res.getString("lastname"), res.getByte("age")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
+        createQuery("TRUNCATE TABLE dbtest.Users;");
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,62 +1,35 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private void createQuery(String query) {
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.executeUpdate(query);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-        createQuery("CREATE TABLE dbtest.Users (\n" +
-                "`id` INT NOT NULL AUTO_INCREMENT,\n" +
-                "`name` VARCHAR(45) NOT NULL,\n" +
-                "`lastname` VARCHAR(45) NOT NULL,\n" +
-                "`age` INT(3) NOT NULL,\n" +
-                "PRIMARY KEY (`id`));");
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        createQuery("DROP TABLE dbtest.Users;");
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        createQuery("INSERT INTO dbtest.Users (name, lastname, age) VALUES " +
-                "('" + name + "', '" + lastName + "', " + age + ");");
-        System.out.println("User с именем - " + name + " добавлен в базу данных.");
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        createQuery("DELETE FROM dbtest.Users WHERE id=" + id + ";");
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        List<User> list = new ArrayList<>();
-        String sql = "SELECT * FROM dbtest.Users";
-        try(Statement statement = Util.getConnection().createStatement()) {
-            ResultSet res = statement.executeQuery(sql);
-            while(res.next()) {
-                list.add(new User(res.getString("name"),
-                        res.getString("lastname"), res.getByte("age")));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return list;
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        createQuery("TRUNCATE TABLE dbtest.Users;");
+        userDaoJDBC.cleanUsersTable();
     }
 }
